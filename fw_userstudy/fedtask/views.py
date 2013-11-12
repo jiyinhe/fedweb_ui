aed9ec55479ce531a6e9b99c0e73bc2d0397a990
he <he@cwi.nl>
2013-11-12 21:15:49 +0100

    modified:   fedtask/views.py 	modified:   templates/fedtask/task_base.html

--- fw_userstudy/fedtask/views.py
+++ fw_userstudy/fedtask/views.py
@@ -111,6 +111,9 @@ def fetch_document(request):
         return response
 
 def clean_html(html):
+	f = open('tmp', 'w')
+	f.write(html)
+	f.close()
 	# Only keep body part
 	reg_body = re.compile('<body.*?>.+?</body>', re.MULTILINE|re.DOTALL)
 	body = reg_body.findall(html)
@@ -123,7 +126,7 @@ def clean_html(html):
 	text = reg_s.sub('', text)
 
 	# clean inputs 
-	reg_f = re.compile('<input.+?>', re.MULTILINE|re.DOTALL)
+	reg_f = re.compile('<input.+?>')
 	text = reg_f.sub('', text)
 	reg_b = re.compile('<button.*?>.*?</button>')
 	text = reg_b.sub('', text)
@@ -140,8 +143,17 @@ def clean_html(html):
 	text_clean = re.sub(r'href=".+?"', '', text)
 
 	# disable images if it's a relative path
-	text_clean = re.sub(r'<img.+?src="\.*/.+?/>', '', text_clean)
-
+	reg_img = re.compile('<img.*?/*>', re.MULTILINE|re.DOTALL)
+	reg_src = re.compile('src=".+?"')	
+	imgs = reg_img.findall(text_clean)
+	for img in imgs:
+		src = reg_src.findall(img)
+		if len(src)>0:
+			if not src[0].split('"')[1].startswith('http'):
+				text_clean = text_clean.replace(img, '')
+	# clean empty lists
+	text_clean = re.sub(r'<li.*?></li>', '', text_clean)
+	# remove 'body' tag
 	text_final =  re.sub(r'<.*?body.*?>', '', text_clean)
 
 	if text_final.strip() == '':
