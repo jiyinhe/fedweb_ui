03888302883f072680a00eb806b5a81dce7014dc
Jiyin He <he@turin.ins.cwi.nl>
2014-01-03 22:33:52 +0100

    new file:   trec_eval/NDCG.py 	new file:   trec_eval/__init__.py 	new file:   trec_eval/trec_util.py

---
+++ scripts/trec_eval/NDCG.py
@@ -0,0 +1,75 @@
+import trec_util
+import sys
+import math
+
+class NDCG:
+
+	# Input the ranked list with relevance judgement
+	# e.g.,  [1, 0, 0, 1] means the 1st and 4th document in the ranked list
+	# are relevant, given binary judgement.
+	# K: ndcg@K
+	def __init__(self, judged_rank):
+		self.judged_rank = judged_rank
+
+	def DCG(self, X, K):
+		if K == -1:
+			length = len(X)
+		else:
+			length = min(K, len(X))
+		
+		# index starts from 1
+		score = [(2**X[i-1]-1)/math.log((i+1)) for i in range(1, length+1)] 
+
+		#print len(score)
+		#Javelin
+		#score = [ X[i]/math.log(i+1, 2) for i in range(1, length)]
+		#score.append(X[0])
+		return sum(score), score	
+
+	def ndcg(self, K):
+		X = self.judged_rank
+		# Sort the whole judgement list
+		iX = sorted(self.judged_rank, reverse=True) 
+		iDCG, score1 = self.DCG(iX, K)
+		DCG, score2 = self.DCG(X, K)
+		#print score2
+		#print DCG, iDCG
+		#print iDCG, DCG
+		if iDCG == 0:
+			return 0
+		return DCG/iDCG
+
+
+if __name__ == '__main__':
+#	X = [3, 2, 3, 0, 1, 2]
+#	n = NDCG(X)
+#	print n.DCG(X, 6), n.DCG(sorted(X, reverse=True), 6), n.ndcg(6)
+	
+	if len(sys.argv)<4:
+		print 'usage python NDCG.py qrels run @K (K=-1 for computing NDCG)'
+		sys.exit()
+
+	qrelfile = sys.argv[1]
+	runfile = sys.argv[2]
+	K = int(sys.argv[3])
+
+	qrels = trec_util.load_qrels(qrelfile)
+	run = trec_util.load_TREC_run(runfile)
+	judged_results = trec_util.judged_ranklist(run, qrels)
+	x = 0
+	sum_x = 0
+	for q in sorted(judged_results.keys()):
+		if q in qrels:
+			j = judged_results[q]
+			# make it binary
+			j = [int(x>0) for x in j]
+			ndcg = NDCG(j)
+			#print q, 
+			#print [(i, judged_results[q][i]) for i in range(len(judged_results[q][0:K]))]
+			#print 'DCG:', ndcg.DCG(judged_results[q], K)
+			#print 'iDCG:', ndcg.DCG(sorted(judged_results[q], reverse=True), K)
+			print q, '%.4f'%ndcg.ndcg(K)	
+			#print
+			sum_x += ndcg.ndcg(K)
+			x += 1
+	print 'mean', sum_x/float(x)
---
+++ scripts/trec_eval/trec_util.py
@@ -0,0 +1,67 @@
+"""
+reading TREC runs and qrels
+"""
+
+import itertools
+import operator
+
+def load_TREC_run(runfile):
+	f = open(runfile)
+	#run = [[[cc[0], cc[2], cc[3], cc[4]] for cc in c.split(' ')] for c in f.readlines()]
+	run = []
+	for c in f:
+		strs = c.strip().split(' ')
+		if len(strs) == 6:
+			run.append([strs[0], strs[2], int(strs[3]), float(strs[4])])
+		else:
+			print 'ERROR in run file: ', strs
+	f.close()
+	per_qry = []
+	# Sort by query	
+	run.sort(key=operator.itemgetter(0))
+	for k, g in itertools.groupby(run, lambda x: x[0]):
+		docs = [d[1:] for d in g]
+		# sort by rank
+		ranklist = sorted(docs, key=lambda x: x[1])
+		# group by query, sorted by doc score, then by docno 
+		#ranklist = sorted(docs, key=lambda x: (-x[2], x[0]))
+		#print [r[1] for r in ranklist], len(ranklist)
+		per_qry.append((k, ranklist))
+	return per_qry
+
+def load_qrels(qrelfile):
+	f = open(qrelfile)
+	q = []
+	for c in f:
+		strs = c.strip().split(' ')
+		if len(strs) == 4:
+			if int(strs[3])>0:
+				q.append((strs[0], strs[2], int(strs[3])))
+		else:
+			print 'ERROR in qrels file'
+			print strs
+	f.close()
+	per_qry = []
+	# Sort by query
+	q.sort(key=operator.itemgetter(0))
+	for k, g in itertools.groupby(q, lambda x: x[0]):
+		docs = [(d[1], d[2]) for d in g]
+		per_qry.append((k, dict(docs)))
+	return dict(per_qry) 
+
+
+def judged_ranklist(run, qrels):
+	res = []
+	for q in run:
+		qid = q[0]
+		docs = [d[0] for d in q[1]]
+		judge = qrels.get(qid, {})
+		judged_list = [judge.get(d, 0) for d in docs]
+
+		#if qid == '7003':
+		#	Q = q[1][0:20]
+	        #	print Q
+		#	print [(i+1, judge.get(Q[i][0], 0)) for i in range(len(Q))]
+
+		res.append((qid, judged_list))
+	return dict(res)	
