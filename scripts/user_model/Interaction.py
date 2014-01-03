9e46d03107ae1f3de3d663c38b376ee246ab95b0
he <he@cwi.nl>
2014-01-03 02:10:00 +0100

    modified:   user_model/Interaction.py

--- scripts/user_model/Interaction.py
+++ scripts/user_model/Interaction.py
@@ -14,18 +14,17 @@ import ExamineModel, FilterModel
 class Interaction:
 	# page_size: number of documents shown in one page?
 	# task_length: number of relevant documents to be found
-	# A large e_labmda represents an impatient user that will stop examining a list early   	
-	# resultlist contains an array of arrays:
-	# - each subarray is a sub ranked list
-	# - each item contains the (doc_id, rank, relevance_judge) 
+	# start: the index of the starting doc (list_id, doc_rank)
+	#
 	# examineModel: instance of the ExamineModel class
 	# filterModel: an instance of the FilterModel class  
 	#
-	def __init__(self, page_size=10, task_length=1, resultlist=[], examineModel, filterModel):
+	def __init__(self, page_size=10, task_length=1, start=[0, 0], examineModel, filterModel):
 		self.page_size = page_size
 		self.K = task_length
 		self.E = examineModel
 		self.F = filterModel
+		self.resultlist = examineModel.resultlist 
 
 		# Assume the first result will always be examined
 		self.N0 = 1
@@ -37,31 +36,26 @@ class Interaction:
 		self.doc_seen = []
 
 		# Total documents
-		self.doc_count = sum([len(d) for d in resultlist]) 
+		self.doc_count = sum([len(d) for d in self.resultlist]) 
 
 		# Number of relevant docs found
 		self.rel_count = 0
 
 		# last visted rank in each sublist
-		self.last_visit = [0 for i in resultlist]
+		self.last_visit = [0 for i in self.resultlist]
 		
 		# the current list the user is at
-		self.current_visit = []
+		self.current_visit = start 
 
 	# Start a simulation
 	def run(self):
 		# Only stop when all documents being examined, or K document has been found   
 		while not (self.rel_count == self.K or self.len(self.doc_seen) == self.doc_count):	
-			# If it's just started, choose a sublist
-			if self.current_list == []:
-				self.choose_list()
-				self.action_list.append('filter')
-		
 			# Now we are at a list, check if next doc has been seen already, if so, skip it
 			if self.doc_is_seen():			
 				continue
 			# Now we are at a new doc, decide if examine another document
-			if self.to_examine_doc():  	
+			if self.examine_next():  	
 				# If the doc is in a next page, first add pagination
 				if self.current_list[1]%self.page_size == 0:
 					self.action_list.append('pagination')
@@ -70,23 +64,25 @@ class Interaction:
 				self.action_list.append('examine')
 			else:
 				# Otherwise do filtering  
-				choose_list()
+				self.choose_list()
+				self.action_list.append('filter')
 
 
 	# Choose a sublist 
 	def choose_list(self):
-		return self.F.get_a_list()
+		return self.F.select_list()
 
 	# Decide if we should examine another doc
-	def to_examine_doc(self):
+	def examine_next(self):
 		# If the current visit position is larger than the maximum docs in the list, then this doc cannot be examined
 		if self.current_visit[1] >= len(self.resultlist[self.current_visit[0]]):
 			return False
 		else:
-			return self.E.to_examine_doc()	
+			return self.E.examine_next()	
 	
 	# Actually examine the doc
 	def examine_doc(self):
+		# Get next doc from current_visit
 		res = resultlist[self.current_visit[0]][self.current_visit[1]] 	
 		docid = res[0]
 
@@ -101,11 +97,15 @@ class Interaction:
 		current_visit[1] = self.current_visit[1] + 1 	
 
 
-	# Check if next doc in the list is already seen, if not, set the current_visit to that doc 
+	# Check if next doc in the list is already seen, 
+	# if not, set the current_visit to that doc 
+	# if yes, skip that doc, set the current_visit to a next doc
 	def doc_is_seen(self):
 		res = resultlist[self.current_visit[0]][self.current_visit[1]] 	
 		docid = res[0]
 		if docid in self.doc_seen:
+			# move the current_visit to next doc in the list
+			current_visit[1] = self.current_visit[1] + 1
 			return True
 		else:	
 			return False
