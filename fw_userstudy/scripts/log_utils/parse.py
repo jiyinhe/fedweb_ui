d3a451ab4a473fd3c6ba88465ab6c60c23938d2a
mbron <m.m.bron@uva.nl>
2014-01-17 22:40:03 +0100

    added some utils to inspect logs

---
+++ fw_userstudy/scripts/log_utils/parse.py
@@ -0,0 +1,27 @@
+import tmp2, sys
+
+hits= tmp2.data['hits']['hits']
+
+time_hit_pairs = [(h['_source']['created_at'],h) for h in hits]
+time_hit_pairs.sort()
+for h in time_hit_pairs:
+	try:
+		data = h[1]['_source']
+		event = data['event_type']
+		if 'event_properties' in data:
+			properties = data['event_properties']
+			if 'node_data_attrs' in properties:
+				print h[0],event,properties['node_data_attrs']
+			elif event=='mouse_movements':
+				for p in properties:
+					print h[0],event,p['node_data_attrs']
+			else:
+				print h[0],event
+		else:
+			print h[0],event
+#[1]['_source']['created_at'], h#['user_id'],h[1]['_source']['event_properties']
+	except:
+		print "************************** missing **********************"
+		print
+
+
