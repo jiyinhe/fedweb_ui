9078cb37f05c016ef08c0c0490aec3c05cdd4d88
mbron <m.m.bron@uva.nl>
2014-01-18 16:59:28 +0100

    added UoG logo and d attribution

--- fw_userstudy/scripts/log_utils/check_log_data.sh
+++ fw_userstudy/scripts/log_utils/check_log_data.sh
@@ -1,4 +1,6 @@
 username="jhe"
+username="mbron"
+username="tttest"
 . test_get_log_data.sh $username | sed 's/null/"null"/g' | sed "s/false/False/g" | sed "s/true/True/g" | sed "s/^{/data={/g" > tmp2.py
 
 python parse.py
