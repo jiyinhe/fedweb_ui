a7f88e8906277631c11966b38ddd4ed8a7cd93dd
mbron <m.m.bron@uva.nl>
2014-01-19 00:37:41 +0100

    added google analytics

--- fw_userstudy/scripts/log_utils/check_log_data.sh
+++ fw_userstudy/scripts/log_utils/check_log_data.sh
@@ -3,7 +3,7 @@ username="mbron"
 username="tttest"
 username="manos"
 #username="dyaaa"
-username="fabrizio"
+#username="fabrizio"
 . test_get_log_data.sh $username | sed 's/null/"null"/g' | sed "s/false/False/g" | sed "s/true/True/g" | sed "s/^{/data={/g" > tmp2.py
 
 python parse.py
