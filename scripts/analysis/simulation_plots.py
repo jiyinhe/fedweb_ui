d3ff5170422bf3a5f01bafcfbe5f5fb2445b8e0d
Jiyin He <he@turin.ins.cwi.nl>
2014-01-20 14:23:49 +0100

    new file:   simulation_plots.py

---
+++ scripts/analysis/simulation_plots.py
@@ -0,0 +1,17 @@
+"""
+analyse the simualtion results
+and generate data for plots, per task type (1, 10, -1)
+
+1. impact of lambda
+2. impact of prior
+3. impact of priorxlambda
+4. impact of dynamic vs. static
+
+"""
+
+
+
+
+
+
+
