a1020ec6af21fe239983f00c0315dfa2db736490
Jiyin He <jiyinhe@Jiyins-MacBook-Pro.local>
2015-01-25 13:09:50 +0100

    save

--- scripts/analysis_0115/smooth_analysis.R
+++ scripts/analysis_0115/smooth_analysis.R
@@ -9,145 +9,146 @@ task1$u_level<-factor(task1$u_quality);
 task10$u_level<-factor(task10$u_quality);
 taskall$u_level<-factor(taskall$u_quality);
 
-# fit the full model
-#mylogit1 <- glm(diff ~ q_difficulty * u_level * f_entropy, data = task1, family = binomial(link="logit"))
-#mylogit10 <- glm(diff ~ q_difficulty * u_level * f_entropy, data = task10, family = binomial(link="logit"))
-#mylogitall <- glm(diff ~ q_difficulty * u_level * f_entropy, data = taskall, family = binomial(link="logit"))
-
-# model selection
-#search<-stepAIC(mylogit1)
-#search$anova
-
-#search<-stepAIC(mylogit10)
-#search$anova
-
-#search<-stepAIC(mylogitall)
-#search$anova
-
 # Fit the model again using the selected model
-mylogit1 <- glm(diff ~ q_difficulty + u_level + f_entropy + q_difficulty:f_entropy + u_level:f_entropy, data=task1, family=binomial(link="logit"))
-mylogit10 <- glm( diff ~ q_difficulty + u_level + f_entropy + q_difficulty:u_level + q_difficulty:f_entropy, data=task10, family=binomial(link="logit")) 
-mylogitall <- glm(diff ~ q_difficulty + u_level + f_entropy + u_level:f_entropy, data=taskall, family=binomial(link="logit"))
+# model 1
+select1.mod<-diff ~ q_difficulty + u_level + f_entropy + q_difficulty:u_level + q_difficulty:f_entropy
+model1<-(glm(select1.mod, data=task1, family=binomial(link = logit)))
+
+# model 10
+select10.mod<-diff ~ q_difficulty + u_level + f_entropy + f_relevance + q_difficulty:f_entropy + q_difficulty:f_relevance + f_entropy:f_relevance + q_difficulty:f_entropy:f_relevance
+model10<-(glm(select10.mod, data=task10, family=binomial(link = logit)))
+
+# model all
+selectall.mode <- diff ~ u_level + f_relevance + q_difficulty + f_entropy + f_relevance:f_entropy + f_relevance:q_difficulty
+modelall<-(glm(selectall.mod, data=taskall, family=binomial(link = logit)))
+
 
 # Generate prediction data for plot
 # create X data points
 # 4 u_levels
-levels<-factor(c(0, 0.02, 0.1, 0.5))
+levels<-factor(c(0, 0.1, 0.5, 1))
 # f_entropy between [c, d]
-dfu<-seq(from=min(task1$f_entropy), to=max(task1$f_entropy), length.out=100)
+entropy<-seq(from=min(task1$f_entropy), to=max(task1$f_entropy), length.out=100)
+# f_relevance 
+relevance<-seq(from=min(task1$f_relevance), to=max(task1$f_relevance), length.out=100)
 # q_difficulty between [a, b], this is different per task
-difficulty_1<-seq(from=min(task1$q_difficulty), to=max(task1$q_difficulty),length.out=100)
-difficulty_10<-seq(from=min(task10$q_difficulty), to=max(task10$q_difficulty),length.out=100)
-difficulty_all<-seq(from=min(taskall$q_difficulty), to=max(taskall$q_difficulty),length.out=100)
+difficulty_1<-seq(from=min(task1$q_difficulty),
+to=max(task1$q_difficulty),length.out=100)
+difficulty_10<-seq(from=min(task10$q_difficulty),
+to=max(task10$q_difficulty),length.out=100)
+difficulty_all<-seq(from=min(taskall$q_difficulty),
+to=max(taskall$q_difficulty),length.out=100)
 
-# make data as combinations of the 3 variables
-data_task1<-expand.grid(difficulty_1, dfu, levels)
-colnames(data_task1)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_task10<-expand.grid(difficulty_10, dfu, levels)
-colnames(data_task10)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_taskall<-expand.grid(difficulty_all, dfu, levels)
-colnames(data_taskall)<-c('q_difficulty', 'f_entropy', 'u_level')
+# Make data for plots -- only consider interactions that were significant
+# Task 1
+# q_difficulty x u_level;  fix f_entropy, f_relevance
+data<-expand.grid(difficulty_1, levels, median(task1$f_entropy),median(task1$f_relevance))
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
 
-# Get predicted data
-# Data for q_difficulty x f_entropy
-tmp1<-cbind(data_task1, predict(mylogit1, newdata=data_task1, type="link", se=TRUE))
-newdata1<-within(tmp1, {
+# model predict
+tmp<-cbind(data, predict(model1, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
+    predictedProb<-plogis(fit) 
+    LL<-plogis(fit-(1.96*se.fit))
+    UL<-plogis(fit+(1.96*se.fit))
+})
+write.table(newdata, 'data/lr_1_qxu.txt')
+
+
+# q_difficulty x f_entropy; fix f_relevance, keep u_levels
+
+# retrain the model to seperate two levels of difficulty
+# recenter q_difficulty to two levels: high & low around 0
+task1$qd_high<-task1$q_difficulty - (mean(task1$q_difficulty)+sd(task1$q_difficulty))
+task1$qd_low<-task1$q_difficulty - (mean(task1$q_difficulty)-sd(task1$q_difficulty))
+# retrain the model to get coefficient of f_entropy at different levels of
+# q_difficulty
+summary(model1)
+select1.mod_high<-diff ~ qd_high + u_level + f_entropy + qd_high:u_level + qd_high:f_entropy
+model1.high<-(glm(select1.mod_high, data=task1, family=binomial(link = logit)))
+summary(model1.high)
+
+select1.mod_low<-diff ~ qd_low + u_level + f_entropy + qd_low:u_level + qd_low:f_entropy
+model1.low<-(glm(select1.mod_low, data=task1, family=binomial(link = logit)))
+summary(model1.low)
+
+# predicted prob with high and low models
+data_high<-expand.
+data<-expand.grid(difficulty_1, levels, entropy, median(task1$f_relevance))
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
+
+# model predict
+tmp<-cbind(data, predict(model1, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
+    predictedProb<-plogis(fit) 
+    LL<-plogis(fit-(1.96*se.fit))
+    UL<-plogis(fit+(1.96*se.fit))
+})
+write.table(newdata, 'data/lr_1_qxfe.txt')
+
+
+
+# Task 10
+# q_difficulty x f_relevance; fix f_entropy, keep u_levels
+data<-expand.grid(difficulty_10, levels, median(task10$f_entropy), relevance)
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
+
+# model predict
+tmp<-cbind(data, predict(model10, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
     predictedProb<-plogis(fit) 
     LL<-plogis(fit-(1.96*se.fit))
     UL<-plogis(fit+(1.96*se.fit))
 })
 
-tmp10<-cbind(data_task10, predict(mylogit10, newdata=data_task10, type="link", se=TRUE))
-newdata10<-within(tmp10, {
+write.table(newdata, 'data/lr_10_qxfr.txt')
+
+
+
+# q_difficulty x f_entropy x f_relevance ; keep u_level
+data<-expand.grid(difficulty_10, levels, entropy, relevance)
+
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
+
+# model predict with model 10
+tmp<-cbind(data, predict(model10, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
     predictedProb<-plogis(fit) 
     LL<-plogis(fit-(1.96*se.fit))
     UL<-plogis(fit+(1.96*se.fit))
 })
 
-tmpall<-cbind(data_taskall, predict(mylogitall, newdata=data_taskall, type="link", se=TRUE))
-newdataall<-within(tmpall, {
+write.table(newdata, 'data/lr_10_4factors.txt')
+
+
+
+# Task all
+# f_entropy x f_relevance; fix difficulty, keep u_level
+data<-expand.grid(median(taskall$q_difficulty), levels, entropy, relevance)
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
+
+# model predict with model all
+tmp<-cbind(data, predict(modelall, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
     predictedProb<-plogis(fit) 
     LL<-plogis(fit-(1.96*se.fit))
     UL<-plogis(fit+(1.96*se.fit))
 })
 
-# save the data
-write.table(newdata1, 'data/lr_3factor_1.txt')
-write.table(newdata10, 'data/lr_3factor_10.txt')
-write.table(newdataall, 'data/lr_3factor_all.txt')
+write.table(newdata, 'data/lr_all_fexfr.txt')
 
+# q_difficulty x f_relevance; fix f_entropy, keep u_level
+data<-expand.grid(difficulty_all, levels, median(taskall$f_entropy), relevance)
+colnames(data)<-c('q_difficulty', 'u_level', 'f_entropy', 'f_relevance')
 
-# Data for f_entropy x u_level, take median of q_difficulty
-data_task1<-expand.grid(median(task1$q_difficulty), dfu, levels)
-colnames(data_task1)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_task10<-expand.grid(median(task10$q_difficulty), dfu, levels)
-colnames(data_task10)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_taskall<-expand.grid(median(taskall$q_difficulty), dfu, levels)
-colnames(data_taskall)<-c('q_difficulty', 'f_entropy', 'u_level')
-
-tmp1<-cbind(data_task1, predict(mylogit1, newdata=data_task1, type="link", se=TRUE))
-newdata1<-within(tmp1, {
+# model predict
+tmp<-cbind(data, predict(modelall, newdata=data, type="link", se=TRUE))
+newdata<-within(tmp, {
     predictedProb<-plogis(fit) 
     LL<-plogis(fit-(1.96*se.fit))
     UL<-plogis(fit+(1.96*se.fit))
 })
 
-tmp10<-cbind(data_task10, predict(mylogit10, newdata=data_task10, type="link", se=TRUE))
-newdata10<-within(tmp10, {
-    predictedProb<-plogis(fit) 
-    LL<-plogis(fit-(1.96*se.fit))
-    UL<-plogis(fit+(1.96*se.fit))
-})
-
-tmpall<-cbind(data_taskall, predict(mylogitall, newdata=data_taskall, type="link", se=TRUE))
-newdataall<-within(tmpall, {
-    predictedProb<-plogis(fit) 
-    LL<-plogis(fit-(1.96*se.fit))
-    UL<-plogis(fit+(1.96*se.fit))
-})
-
-# save the data
-write.table(newdata1, 'data/lr_fxu_1.txt')
-write.table(newdata10, 'data/lr_fxu_10.txt')
-write.table(newdataall, 'data/lr_fxu_all.txt')
-
-# Data for q_difficulty x u_level, take median of f_entropy
-data_task1<-expand.grid(difficulty_1, median(task1$f_entropy), levels)
-colnames(data_task1)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_task10<-expand.grid(difficulty_10, median(task10$f_entropy), levels)
-colnames(data_task10)<-c('q_difficulty', 'f_entropy', 'u_level')
-data_taskall<-expand.grid(difficulty_all, median(taskall$f_entropy), levels)
-colnames(data_taskall)<-c('q_difficulty', 'f_entropy', 'u_level')
-
-
-tmp1<-cbind(data_task1, predict(mylogit1, newdata=data_task1, type="link", se=TRUE))
-newdata1<-within(tmp1, {
-    predictedProb<-plogis(fit) 
-    LL<-plogis(fit-(1.96*se.fit))
-    UL<-plogis(fit+(1.96*se.fit))
-})
-
-tmp10<-cbind(data_task10, predict(mylogit10, newdata=data_task10, type="link", se=TRUE))
-newdata10<-within(tmp10, {
-    predictedProb<-plogis(fit) 
-    LL<-plogis(fit-(1.96*se.fit))
-    UL<-plogis(fit+(1.96*se.fit))
-})
-
-tmpall<-cbind(data_taskall, predict(mylogitall, newdata=data_taskall, type="link", se=TRUE))
-newdataall<-within(tmpall, {
-    predictedProb<-plogis(fit) 
-    LL<-plogis(fit-(1.96*se.fit))
-    UL<-plogis(fit+(1.96*se.fit))
-})
-
-# save the data
-write.table(newdata1, 'data/lr_qxu_1.txt')
-write.table(newdata10, 'data/lr_qxu_10.txt')
-write.table(newdataall, 'data/lr_qxu_all.txt')
-
-
-
-
+write.table(newdata, 'data/lr_all_qxfr.txt')
 
 
--- scripts/analysis_0115/smooth_effect.py
+++ scripts/analysis_0115/smooth_effect.py
@@ -22,9 +22,11 @@ import itertools
 from Configure import generalDir, util
 
 datadir_basic = '../../data/simulation_basic/'
-datadir_smooth = '../../data/simulation_smooth2/'
+datadir_smooth = '../../data/simulation_smooth3/'
 outdir = 'data/'
 
+# only consider these user accuracy parameters
+u_levels = ['0', '0.1', '0.5', '1']
 
 runfile = generalDir.runfile
 qrelsfile = generalDir.qrelsfile
@@ -42,44 +44,39 @@ def make_data(basic_data, smooth_data, pa_b, pa, facet_entropy):
         b_data =  basic_data[b]
         # find correponding faceted simulation with the same task
         task_p = list(itertools.ifilter(lambda x: x[5]==task, pa))
-        D = [','.join(['diff', 'f_entropy', 'q_difficulty', 'u_quality'])]
+        D = [','.join(['diff', 'f_entropy', 'f_relevance', 'q_difficulty', 'u_quality'])]
         for p in task_p:
             idx = p[0]
             s_data = smooth_data[idx]
             # independent variable  user level
-            u_level = p[-1] 
+            u_level = p[-1]
+            if not u_level in u_levels:
+                continue
             for q in s_data:
                 # independent variable query difficulty
                 basic_performance = b_data[q][0]
                 # independent varialbe facet entropy
-                entropy = facet_entropy[q]
+                entropy, mean_ndcg = facet_entropy[q]
                 # dependent variable diff
                 diff = b_data[q] - np.median(s_data[q]) 
                 diff = int(diff[0]>0)
-                line = [str(diff), str(entropy), str(basic_performance), str(u_level)]
+
+                line = [str(diff), str(entropy), str(mean_ndcg), str(basic_performance), str(u_level)]
                 D.append(','.join(line))
         f = open('%s/smooth_data_task%s.csv'%(outdir, task), 'w')
         f.write('\n'.join(D))
         f.close() 
 
  
-# KL divergence of distribution of relevant docs
-# among sublists from a uniform distribution
-# We remove the first list from ths sublists, 
-# as it is the original list. 
-def sublist_entropy(sublists):
-    p_random = 1./len(sublists[1:])
-    rel_counts = [sum([int(s[2]>0) for s in sub]) for sub in sublists[1:]] 
-    tot_rel = sum(rel_counts)
-    H_P = 0
-    H_PQ = 0
-    for count in rel_counts:
-        p_rel = float(count)/tot_rel
-        if p_rel > 0:
-            H_P += - (p_rel * log(p_rel))
-            H_PQ += -(p_rel * log(p_random))
-    return H_PQ - H_P 
-
+# Entropy of NDCG scores
+def sublist_entropy(scores):
+    tot_score = sum(scores)
+    H = 0
+    for s in scores:
+        if s > 0:
+            p = s/tot_score
+            H += - p * log(p)
+    return H
 
 # Prepare data to describe the properties of the sublists
 # hypothesis:
@@ -99,8 +96,12 @@ def prepare_data_sublists():
             continue
         judge = judged_list.get(q[0])
         sublists, B, G = u.create_sublists(docs, judge, categories)
-        entropy = sublist_entropy(sublists)
-        facets_data[q[0]] = entropy  
+
+        ndcg_scores = [b[-1] for b in B]
+        entropy = sublist_entropy(ndcg_scores)
+        mean_ndcg_scores = np.mean(ndcg_scores)
+
+        facets_data[q[0]] = (entropy, mean_ndcg_scores)
     return facets_data 
 
 
